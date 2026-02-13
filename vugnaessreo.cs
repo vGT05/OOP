58c24b6a22fd6040b0bb9adb1b3521475d3f03ae
vGT05 <roserianriva090@gmail.com>
2026-02-13 19:20:20 -0300

    Aula 3

--- vugnaessreo.cs
+++
@@ -1,8 +0,0 @@
-using System; //Biblioteca do C# (system) para o usar comandos do Console
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("que trampo");
-    }
-}
