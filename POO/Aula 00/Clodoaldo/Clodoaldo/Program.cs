76de5e45f30e98ffe7fe3030e5b32222e563a178
vGT05 <roserianriva090@gmail.com>
2026-03-18 18:46:13 -0300

    aula 18.03.2026

---
+++ POO/Aula 00/Clodoaldo/Clodoaldo/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+int a, b, resultado;
+WriteLine("Digite dois números.");
+Write("Primeiro número: ");
+a = int.Parse(ReadLine());
+Write("Segundo número: ");
+b = int.Parse(ReadLine());
+resultado = (a + b);
+WriteLine($"O restultado é {resultado}");
---
+++ POO/Aula 01/ExemploComOOP/ExemploComOOP/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿namespace ExemploComOOP
+{
+    internal class Triangulo
+    {
+        //Campos/atributos
+        public double ladoA, ladoB, ladoC;
+
+        //Método
+        public double Area()
+        {
+            double p = (ladoA + ladoB + ladoC) / 2;
+            double area = Math.Sqrt(p * (p - ladoA) * (p - ladoB) * (p - ladoC));
+            return area;
+        }
+
+    }
+}
